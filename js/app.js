f11de0b88eda57203b18ce35f2577c65e95af105
Otto Vosberg <o.vosberg@gmail.com>
2016-05-14 21:11:19 -0500

    updates

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
-var jQuery = require('jquery');
-var React = require('react');
-var $ = jQuery;
+window.jQuery = require('jquery');
+var $ = window.jQuery;
 var App = require('./components');
 
-React.render(<App.Views.Hello/>, document.getElementById('test'));
+$(document).ready(function() {
+    
+    $(window).scroll( function(){
+    
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
