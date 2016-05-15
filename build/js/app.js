55f1e2f28bee917e3e259d8d5d119d64c2890ea2
Otto Vosberg <o.vosberg@gmail.com>
2016-05-14 21:05:35 -0500

    updates

---
+++ build/js/app.js
@@ -0,0 +1,5 @@
+window.jQuery = require('jquery');
+var $ = window.jQuery;
+var App = require('./components');
+
+var Quote = new App.Views.QOD;
---
+++ build/js/components/views.js
@@ -0,0 +1,42 @@
+var Backbone = require('backbone');
+var jQuery = require('jquery');
+var $ = jQuery;
+var _ = require('underscore');
+
+var QODModel = Backbone.Model.extend();
+var QODCollection = Backbone.Collection.extend({
+    model:QODModel,
+    url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
+    parse: function(response, xhr){
+        return response;
+    },
+    //sync: function(method, model, options) {
+            //var that = this;
+                //var params = _.extend({
+                    //type: 'GET',
+                    //dataType: 'jsonp',
+                    //url: that.url,
+                    //processData: false
+                //}, options);
+
+            //return $.ajax(params);
+        //}
+});
+var QOD = Backbone.View.extend({
+    initialize: function(){
+        this.collection = new QODCollection;
+        var that = this;
+        this.collection.fetch({
+            success:function(){
+                that.render();
+            }
+        });
+    },
+    render: function(){
+        console.log(this.collection);
+        $('#slide-content').html(this.collection.models[0].attributes.content);
+    }
+});
+
+
+module.exports = {QOD};
